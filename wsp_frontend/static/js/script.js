9126a69c6e85fcf4b8cd885498a8790ee3d9c8de
Sascha <grabsch@bbaw.de>
2014-05-21 12:37:45 +0200

    Statistik für Frontpage, implementiert mit nvd3.js/d3.js

--- wsp_frontend/static/js/script.js
+++ wsp_frontend/static/js/script.js
@@ -27,3 +27,25 @@ $("#search-results li").mouseover(function() {
 */
 
 $(".previewLink").colorbox({iframe:true, width:"500px", innerHeight:"550px"});    
+
+ window.onload = function () {
+ console.log("Running Raphael...")
+ var r = Raphael("projekte_stat");
+ data = [7763, 5052, 1708, 920, 712];
+ projekte = ["Sekundärliteraturdatenbank zu Alexander von Humboldt", "Inscriptiones Graecae", "Registres de l'Academie", "Deutsches Textarchiv", "Die unselbständigen Schriften Alexander von Humboldts"];
+ r.hbarchart(0, 0, 300, 190, data, { stacked: false });
+ console.log(r);
+ console.log("Done.");
+
+ r = null;
+ 
+ console.log("Running Raphael...");
+ var r = Raphael("autoren_stat");
+ console.log(r);
+ autoren_data = [1019, 293, 165, 108, 81];
+ autoren = ["Humboldt, Alexander von", "Biermann, Kurt-Reinhard", "Schwarz, Ingo", "Beck, Hanno", "Ette, Ottmar"];
+ r.hbarchart(0, 0, 300, 190, autoren_data, { stacked: false });
+ console.log(r);
+ console.log("Done.")
+ //alert("Test");
+ };
--- wsp_frontend/wsp_frontend/settings/local.py
+++ wsp_frontend/wsp_frontend/settings/local.py
@@ -4,14 +4,14 @@ from .base import *
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    "C:/Dokumente und Einstellungen/sgrabsch/Eigene Dateien/wspdev/wspSite/wsp_frontend/static/",
+    "C:/Users/grabsch/wspdev/wspSite/wsp_frontend/static/",
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
 
 TEMPLATE_DIRS = (
-    "C:/Dokumente und Einstellungen/sgrabsch/Eigene Dateien/wspdev/wspSite/wsp_frontend/templates",
+    "C:/Users/grabsch/wspdev/wspSite/wsp_frontend/templates",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
