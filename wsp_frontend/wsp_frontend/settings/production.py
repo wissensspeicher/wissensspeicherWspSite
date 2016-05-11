3b44ff21ec5af9c4c5e8d3a3f73533c31d3c6fac
Sascha Grabsch <grabsch@bbaw.de>
2016-05-11 12:26:53 +0200

    Change configuration for production deployment
    https://telotadev.bbaw.de/redmine/issues/5563
    
    As part of updating Django to 1.9 on the new production
    server (<wissensspeicher.bbaw.de>) some paths needed to
    be adjusted. We still need to split up the configuration
    for for the servers (development vs production), e. g.
    for DEBUG mode and maybe logging.

--- wsp_frontend/wsp_frontend/settings/production.py
+++ wsp_frontend/wsp_frontend/settings/production.py
@@ -4,14 +4,14 @@ from .base import *
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    "/home/sascha/wsp_frontend/static",
+    "/home/sascha/wspSite/wsp_frontend/static",
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
 
 TEMPLATE_DIRS = (
-    "/home/sascha/wsp_frontend/templates",
+    "/home/sascha/wspSite/wsp_frontend/templates",
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
