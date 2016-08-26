f69539c324cd759c074a6f181f2639dc8c1733e7
Sascha Grabsch <grabsch@bbaw.de>
2016-08-26 13:24:09 +0200

    Add robots.txt file & corresponding URL routing
    This fixes https://telotadev.bbaw.de/redmine/issues/6775.

--- wsp_frontend/wsp_frontend/urls.py
+++ wsp_frontend/wsp_frontend/urls.py
@@ -1,27 +1,16 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-#from wsp_site.views import hello_world, search
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'wsp_frontend.views.home', name='home'),
-    # url(r'^wsp_frontend/', include('wsp_frontend.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
-
-urlpatterns = patterns('',
-    url('^search/', 'webpages.views.search', name = 'search'),
-    url('^impressum/', 'webpages.views.impressum', name = 'impressum'),
-    ('^$', 'webpages.views.hello_world'),
-    (r'^status/details', 'status.views.status_details'),
-    (r'^status/', 'status.views.status'),
-)
+                       url('^search/', 'webpages.views.search', name='search'),
+                       url('^impressum/', 'webpages.views.impressum', name='impressum'),
+                       ('^$', 'webpages.views.hello_world'),
+                       (r'^status/details', 'status.views.status_details'),
+                       (r'^status/', 'status.views.status'),
+                       (r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+                       )
